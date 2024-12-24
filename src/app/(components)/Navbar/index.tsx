b6c7f4b295619443319306ef194aef3190f1f6b5
Leon <tahjib.leon399@gmail.com>
2024-12-24 18:38:27 +0600

    Dark mode enable

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -1,13 +1,29 @@
 import React, { FC } from 'react';
-import { Search, Settings } from 'lucide-react'
+import { MenuIcon, Moon, Search, Settings, Sun } from 'lucide-react'
 import Link from 'next/link';
+import { useAppDispatch, useAppSelector } from '@/app/redux';
+import { setIsDarkMode, setIsSidebarCollapsed } from '@/state';
+import clsx from 'clsx';
 
 
 const Navbar: FC = () => {
+    const dispatch = useAppDispatch();
+    const isSidebarCollapsed = useAppSelector((state: any) => state.global.isSidebarCollapsed);
+    const isDarkMode = useAppSelector((state: any) => state.global.isDarkMode);
+
     return (
         <div className='flex items-center justify-between bg-white px-4 py-3 dark:bg-dark-bg'>
             {/* //!Left part  */}
             <div className='flex items-center gap-8'>
+                {
+                    !isSidebarCollapsed ? null : (
+                        <button
+                            onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}
+                        >
+                            <MenuIcon className='h-8 w-8 dark:text-white' />
+                        </button>
+                    )
+                }
                 <div className='relative flex h-min w-[200px]'>
                     <Search
                         className='absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white'
@@ -23,7 +39,21 @@ const Navbar: FC = () => {
 
             {/* //!Right part  */}
             <div className="flex items-center">
-                <Link href="/settings" className='h-min w-min rounded p-2 hover:bg-gray-100'>
+                <button
+                    onClick={() => dispatch(setIsDarkMode(!isDarkMode))}
+                    className={clsx(`rounded p-2`, isDarkMode ? 'dark:hover:bg-gray-700' : 'hover:bg-gray-100')}
+                >
+                    {
+                        isDarkMode ? (
+                            <Sun className='h-6 w-6 cursor-pointer dark:text-white' />
+                        ) : (
+                            <Moon className='h-6 w-6 cursor-pointer dark:text-white' />
+                        )
+                    }
+                </button>
+                <Link href="/settings"
+                    className={clsx(`rounded p-2 h-min w-min`, isDarkMode ? 'dark:hover:bg-gray-700' : 'hover:bg-gray-100')}
+                >
                     <Settings className='h-6 w-6 cursor-pointer dark:text-white' />
                 </Link>
                 <div className="ml-2 mr-5 hidden min-h-[2em] w-[0.1rem] bg-gray-200 md:inline-block"></div>
--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,22 +1,45 @@
-import React, { FC } from 'react'
+'use client'
+import React, { FC, useEffect } from 'react'
 import Navbar from './(components)/Navbar'
 import Sidebar from './(components)/Sidebar'
+import StoreProvider, { useAppSelector } from './redux'
 
 interface Props {
     children: React.ReactNode
 }
 
-const DashboardWrapper: FC<Props> = ({ children }) => {
+const DashboardLayout: FC<Props> = ({ children }) => {
+
+    const isSidebarCollapsed = useAppSelector((state: any) => state.global.isSidebarCollapsed);
+    const isDarkMode = useAppSelector((state: any) => state.global.isDarkMode);
+
+    useEffect(() => {
+        if (isDarkMode) {
+            document.documentElement.classList.add('dark')
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
+
+    }, [isDarkMode])
+
     return (
         <div className='flex min-h-screen w-full bg-gray-50'>
             {/* Sidebar  */}
             <Sidebar />
-            <main className={`w-full flex flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
+            <main className={`w-full flex flex-col bg-gray-50 dark:bg-dark-bg ${isSidebarCollapsed ? '' : 'ml-64'}`}>
                 {/*  Navbar  */}
                 <Navbar />
                 {children}
             </main>
         </div>
+    )
+}
+
+const DashboardWrapper: FC<Props> = ({ children }) => {
+    return (
+        <StoreProvider>
+            <DashboardLayout>{children}</DashboardLayout >
+        </StoreProvider >
     )
 }
 
